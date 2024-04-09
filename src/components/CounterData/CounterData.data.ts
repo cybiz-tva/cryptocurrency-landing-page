88ec46df2de165252652439e18f4968557c0815a
cybiz <49467492+cybiz-tva@users.noreply.github.com>
2024-04-09 19:40:32 +0530

    text

--- src/components/CounterData/CounterData.data.ts
+++ src/components/CounterData/CounterData.data.ts
@@ -3,9 +3,9 @@ export const dataCounterData = [
     id: 1,
     startNumber: 120,
     endNumber: 3800,
-    text: "Active Users",
+    text: "Active Holders",
   },
-  { id: 2, startNumber: 0, endNumber: 120, text: "Nodes Worldwide" },
+  { id: 2, startNumber: 0, endNumber: 800000000, text: "Total Supply" },
   {
     id: 3,
     startNumber: 100,
