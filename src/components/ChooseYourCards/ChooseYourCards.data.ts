c24f1b9121fe9e6f974f79770b972c5c9407e398
cybiz <49467492+cybiz-tva@users.noreply.github.com>
2024-04-11 00:46:44 +0530

    cards

--- src/components/ChooseYourCards/ChooseYourCards.data.ts
+++ src/components/ChooseYourCards/ChooseYourCards.data.ts
@@ -11,4 +11,12 @@ export const dataCards = [
     id: 3,
     image: "/assets/card-3.png",
   },
+  {
+    id: 4,
+    image: "/assets/card-4.png",
+  },
+  {
+    id: 5,
+    image: "/assets/card-5.png",
+  },
 ];
