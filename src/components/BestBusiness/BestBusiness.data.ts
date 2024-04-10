f40bd43ab957a2340d38e8c443179d02abebdf0a
cybiz <49467492+cybiz-tva@users.noreply.github.com>
2024-04-11 01:01:06 +0530

    texts

--- src/components/BestBusiness/BestBusiness.data.ts
+++ src/components/BestBusiness/BestBusiness.data.ts
@@ -4,21 +4,21 @@ export const dataFeaturesBusiness: BestBusinessData = [
   {
     id: 1,
     icon: "star",
-    title: "Recompensas",
+    title: "Decentralization and Trust",
     description:
       " We have developed groundbreaking living technologies, including a zero-energy body calibration device. This innovation facilitates mobility and improves quality of life by helping individuals with physical illnesses and the elderly.",
   },
   {
     id: 2,
     icon: "shield",
-    title: "100% Seguro",
+    title: "Secure and Efficient Transactions",
     description:
       "Our e-commerce systems integrate seamlessly with different brands, offering customers the opportunity to shop using our own cryptocurrencies. With the global crypto trading market projected to reach $3.1 trillion by 2030, our active token serves as the gateway to a new era of shopping.",
   },
   {
     id: 3,
     icon: "send",
-    title: "Envíos gratis",
+    title: "Transparency and Authenticity",
     description:
       "Exclusive Discounts and Promotions: Holders of RAFEE tokens enjoy special discounts and promotions when making purchases on our platform, encouraging adoption and usage of the token.",
   },
